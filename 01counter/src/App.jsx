09904b6488b151d13280abdfa68576fe4401a5ca
Your Name <you@example.com>
2025-07-31 11:01:38 +0530

    counter project

---
+++ 01counter/src/App.jsx
@@ -0,0 +1,43 @@
+
+import { useState } from 'react';
+import './App.css'
+
+function App() {
+
+ let [counter , setCounter] = useState(0)
+  
+//let counter = 15;
+const addValue = () => {
+   if (counter!==20) {
+    setCounter(counter + 1)
+  } else {
+    setCounter(counter)
+  }
+
+}
+const removeValue =() => {
+  if (counter!==0) {
+    setCounter(counter - 1)
+  } else {
+    setCounter(counter)
+  }
+  
+  
+}
+  return (
+    <>
+      <h1>Chai aur react</h1>
+      <h2>Counter value: {counter}</h2>
+      <button
+      onClick={addValue}
+       
+       >Add value: {counter}</button><br/>
+      <button
+      onClick={removeValue}
+      >Remove value: {counter}</button>
+      <p>footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
