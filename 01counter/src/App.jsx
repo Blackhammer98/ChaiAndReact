ee9a3386b6de52c98f9302b05d153c6b1dc161ea
Your Name <you@example.com>
2025-08-07 10:48:12 +0530

    interview

--- 01counter/src/App.jsx
+++ 01counter/src/App.jsx
@@ -1,43 +1,34 @@
-
-import { useState } from 'react';
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  let [counter, setCounter] = useState(0);
 
- let [counter , setCounter] = useState(0)
-  
-//let counter = 15;
-const addValue = () => {
-   if (counter!==20) {
-    setCounter(counter + 1)
-  } else {
-    setCounter(counter)
-  }
-
-}
-const removeValue =() => {
-  if (counter!==0) {
-    setCounter(counter - 1)
-  } else {
-    setCounter(counter)
-  }
-  
-  
-}
+  //let counter = 15;
+  const addValue = () => {
+    if (counter !== 20) {
+      setCounter(counter + 1);
+    } else {
+      setCounter(counter);
+    }
+  };
+  const removeValue = () => {
+    if (counter !== 0) {
+      setCounter(counter - 1);
+    } else {
+      setCounter(counter);
+    }
+  };
   return (
     <>
       <h1>Chai aur react</h1>
       <h2>Counter value: {counter}</h2>
-      <button
-      onClick={addValue}
-       
-       >Add value: {counter}</button><br/>
-      <button
-      onClick={removeValue}
-      >Remove value: {counter}</button>
+      <button onClick={addValue}>Add value: {counter}</button>
+      <br />
+      <button onClick={removeValue}>Remove value: {counter}</button>
       <p>footer: {counter}</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
